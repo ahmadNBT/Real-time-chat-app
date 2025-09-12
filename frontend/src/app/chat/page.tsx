a3de2e627c889f4127a9680fb85db17fb42516d1
ahmadNBT <ahmadnbt21@gmail.com>
2025-09-12 18:40:54 +0500

    working on chats messages UI

--- frontend/src/app/chat/page.tsx
+++ frontend/src/app/chat/page.tsx
@@ -2,9 +2,13 @@
 
 import ChatSidebar from "@/components/chatSidebar";
 import Loading from "@/components/Loading";
-import { useAppData, User } from "@/context/AppContext";
+import { chat_service, useAppData, User, user_service } from "@/context/AppContext";
 import { useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
+import toast from "react-hot-toast";
+import Cookies from "js-cookie";
+import axios from "axios";
+import ChatHeader from "@/components/chatHeader";
 
 export interface Message {
   _id: string;
@@ -55,6 +59,54 @@ const Chat = () => {
 
   const handleLogout = () => logoutUser();
 
+  async function fetchChat() {
+
+    try {
+      const token = Cookies.get("token");
+      const { data } = await axios.get(`${chat_service}/api/v1/message/${selectedUser}`, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      })
+      setMessages(data.messages)
+      setUser(data.user)
+      await fetchChats();
+    } catch (error) {
+      console.log(error);
+      toast.error("failed to load messages");
+    }
+
+  }
+
+  async function createChat(u: User) {
+      try {
+
+        const token = Cookies.get("token");
+
+        const {data} = await axios.post(`${chat_service}/api/v1/chat/new`, {
+          otherUserId: u._id
+        }, {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        })
+        setSelectedUser(data?.chatId)
+        setShowAllUsers(false)
+
+        await fetchChats();
+
+      } catch (error) {
+        console.log(error);
+        toast.error("failed to create chat");
+      }
+  }
+
+  useEffect(() => {
+    if(selectedUser) {
+      fetchChat()
+    }
+  }, [selectedUser])
+
   if (loading) return <Loading />;
 
   return (
@@ -70,7 +122,13 @@ const Chat = () => {
         selectedUser={selectedUser}
         setSelectedUser={setSelectedUser}
         handleLogout={handleLogout}
+        createChat={createChat}
       />
+
+    <div className="flex-1 flex flex-col justify-between p-4 backdrop-blur-xl bg-white/5 border-1 border-white/10 ">
+      <ChatHeader />
+    </div>
+
     </div>
   );
 };
